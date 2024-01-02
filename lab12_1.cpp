18b9ccce9d0c3700c1de6af0b5b8d760028cb0fa
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-01-02 08:12:29 +0700

    lab12.1 Complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int x){
+    if (x == 0) return 0;
+    if (x == 1) return 1;
+    else if (x > 1) return fibonacci(x-1) + fibonacci(x-2);
+    
+    return x;
+}
+
