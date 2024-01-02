77c8a05909acb596b166318d2699d87a9d8a8d53
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-01-02 09:00:09 +0700

    lab12.2 Complete

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    if (x%y == 0)
+    {
+        return y;
+    }else if (x%y !=0)
+    {
+        return gcd(y, x%y);
+    }
+
+    return x;
+    
 }
