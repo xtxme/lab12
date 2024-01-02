694f0d0e4ffb8274e2a01a983c0dddf90582af07
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2024-01-02 08:36:49 +0700

    lab12.1 Re-commit complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
+#include <ctime>
 using namespace std;
 
-int fibonacci(int x);
+long long fibonacci(int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int x){
+long long fibonacci(int x){
     if (x == 0) return 0;
     if (x == 1) return 1;
     else if (x > 1) return fibonacci(x-1) + fibonacci(x-2);
     
     return x;
 }
-
